5aa52daca0a741fc0b33d4be49c1457304897c08
Yevgeniy <yevgeniy.shin.v@gmail.com>
2022-10-25 11:31:04 +0600

    initial commit

---
+++ familyForm/familyForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  familyForm
+//
+//  Created by boockich mac on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
